eb5f7b33343b2bd97143623e38bb35d826de1519
Rahmat-Hiday <rahmat.hiday92@gmail.com>
2022-11-11 01:39:19 +0700

    update2

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,25 +1,42 @@
-import { Button, Form } from "react-bootstrap"
+import { Button, Form } from "react-bootstrap";
 
 const InputTodo = ({ handleChange, handleSubmit, todo, edit }) => {
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3  w-50 mx-auto" controlId="exampleForm.ControlInput1">
-                <Form.Label>Title todo</Form.Label>
-                <Form.Control value={todo.title || ""} name="title" onChange={handleChange} type="text" placeholder="Masukan title todo"
-                    required
-                />
-            </Form.Group>
-            <Form.Group className="mb-3 w-75 mx-auto" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Description todo</Form.Label>
-                <Form.Control value={todo.desc || ""} name="desc" onChange={handleChange} as="textarea" rows={3} placeholder="Masukan deskripsi"
-                    required
-                />
-            </Form.Group>
-            <Button type="submit" className="border mx-auto d-block">
-                {edit ? "Edit todo" : "Create todo"}
-            </Button>
-        </Form>
-    )
-}
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group
+        className="mb-3  w-50 mx-auto"
+        controlId="exampleForm.ControlInput1"
+      >
+        <Form.Label>Title todo</Form.Label>
+        <Form.Control
+          value={todo.title || ""}
+          name="title"
+          onChange={handleChange}
+          type="text"
+          placeholder="Masukan title todo"
+          required
+        />
+      </Form.Group>
+      <Form.Group
+        className="mb-3 w-75 mx-auto"
+        controlId="exampleForm.ControlTextarea1"
+      >
+        <Form.Label>Deskripsi TODO</Form.Label>
+        <Form.Control
+          value={todo.desc || ""}
+          name="desc"
+          onChange={handleChange}
+          as="textarea"
+          rows={3}
+          placeholder="Masukan deskripsi"
+          required
+        />
+      </Form.Group>
+      <Button type="submit" className="border mx-auto d-block">
+        {edit ? "Edit todo" : "Create todo"}
+      </Button>
+    </Form>
+  );
+};
 
-export default InputTodo
+export default InputTodo;
